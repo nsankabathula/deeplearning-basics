9908a59f52e39be0bbab4ffb869efa38222e007e
nsankabathula <naveen.c.s@hotmail.com>
2018-01-24 15:44:17 -0500

    setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='TensorFlow',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='nsankabathula',
+    author_email='naveen.c.s@hotmail.com',
+    description='',
+    install_requires=['tensorflow', 'numpy', 'scipy', 'matplotlib', 'ipython', 'pandas', 'sympy','nose'] #external packages as dependencies
+)
