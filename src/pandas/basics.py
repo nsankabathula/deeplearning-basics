5602775d8495cd7c83514235329858e897360780
nsankabathula <naveen.c.s@hotmail.com>
2018-02-01 16:21:53 -0500

    pandas changes

--- src/pandas/basics.py
+++ src/pandas/basics.py
@@ -305,3 +305,25 @@ df = pd.DataFrame(exam_data, index=labels);
 #print (df.sort_values(ascending=False,by='name').sort_values(ascending=True, by='score'));
 print ('Actual Solution:\n', df.sort_values(ascending=[False,True],by=['name','score']));
 print ("\n=======================End=====================\n ")
+
+
+print ("\n****************** DataFrame Test 17 ***********************\n ")
+exam_data = {
+    'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+    'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+    'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+    'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']
+    }
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+
+
+df = pd.DataFrame(exam_data, index=labels);
+
+#print('!!! INCOMPLETE !!!');
+df['qualify'] = df['qualify'].map({'yes': True, 'no': False});
+
+print ('Solution: \n', df);
+
+
+print ("\n=======================End=====================\n ")
